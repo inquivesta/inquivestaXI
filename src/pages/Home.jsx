82bca98e640edef6ba07e1ed12e5d8cfc1efaf49
TheSillyCoder <sarkardebayan45@gmail.com>
2025-01-05 18:41:04 +0530

    dates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
     <Navbar/>
     <div className="hero-section">
       <img src="./logo.png" alt="" className="hero-img" />
+      <h3 className='glitch'>7th to 9th February, 2025</h3>
       <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.95}} transition={{ type: "spring", stiffness: 400, damping: 10 }}><Link to="/about" className='hero-link'>But What is Inquivesta?</Link></motion.div>
     </div>
     </>
