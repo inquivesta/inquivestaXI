fa253e0d504492fc1049f82de32cbe040530fb8a
Sattwamo Ghosh <sattwamoghosh@gmail.com>
2025-01-13 20:30:05 +0530

    started firebase integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,62 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Events from './pages/Events.jsx'
-import Sponsors from './pages/Sponsors.jsx'
-import Contact from './pages/Contact.jsx'
-import Schedule from './pages/Schedule.jsx'
-import Team from './pages/Team.jsx'
-import Merch from './pages/Merch.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Events from "./pages/Events.jsx";
+import Sponsors from "./pages/Sponsors.jsx";
+import Contact from "./pages/Contact.jsx";
+import Schedule from "./pages/Schedule.jsx";
+import Team from "./pages/Team.jsx";
+import Merch from "./pages/Merch.jsx";
+import Test from "./pages/Test.jsx";
+import Register from "./pages/Register.jsx";
 
 const router = createHashRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Home />,
   },
   {
     path: "/about",
-    element: <About/>
+    element: <About />,
   },
   {
     path: "/events",
-    element: <Events/>
+    element: <Events />,
   },
   {
     path: "/sponsors",
-    element: <Sponsors/>
+    element: <Sponsors />,
   },
   {
     path: "/contact",
-    element: <Contact/>
+    element: <Contact />,
   },
   {
     path: "/schedule",
-    element: <Schedule/>
+    element: <Schedule />,
   },
   {
     path: "/team",
-    element: <Team/>
+    element: <Team />,
   },
   {
     path: "/merch",
-    element: <Merch/>
-  }
-])
-createRoot(document.getElementById('root')).render(
+    element: <Merch />,
+  },
+  {
+    path: "/test",
+    element: <Test/>
+  },
+  {
+    path: "/register/*",
+    element: <Register />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
