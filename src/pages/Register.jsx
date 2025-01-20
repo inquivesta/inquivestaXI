0561f2d7b2a56aa3fc9e0bded671f0b6a050be1f
TheSillyCoder <sarkardebayan45@gmail.com>
2025-01-21 00:33:47 +0530

    event deets some

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -27,7 +27,7 @@ const Register = () => {
       ...prevValues, 
       [name]: value, 
     }));
-    console.log(dropdownValues)
+    // console.log(dropdownValues)
 
   }
 
@@ -56,14 +56,15 @@ const Register = () => {
 
    ],
    
-     CSI: [...defaultForm, 
+    CSI: [...defaultForm, 
        "institute",
        {name: "members", ele: textarea("Other Team Members (1 - 4)", "members", "Provide one to four names")},
        {name: "leader", ele: textarea("Team Leader", "leader", "Name of a Team Leader")},
        {name: "teamname", ele: textarea("Team Name", "teamname", "Team Name")},
        {name: "amt", ele: information("Amount payable: INR 200")},
        {name: "utr", ele: textarea("Enter full UTR number", "utr", "UTR goes here")},
-   ]
+   ],
+    ART_IN_A_CULTURE: [...defaultForm, "institute"],
  }
  
 
@@ -97,7 +98,11 @@ const Register = () => {
       const data = await response.json();
       alert(data["message"]);
       // navigate("/events");
-      window.open(data["qrcode"], "_blank")
+      if (data["qrcode"]) {
+        window.open(data["qrcode"], "_blank")
+      } else {
+        navigate("/events");
+      }
       // console.log(data["qrcode"])
     } catch (error) {
       console.error("Error posting data:", error);
